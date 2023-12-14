1df0c59c273dfa8ccc4e9996730f8e9785257841
Luke Hewitt <lukehewitt@smashit.games>
2023-12-14 21:24:33 +1000

    on the right track with regex but like..
    not processing every line?

--- 1/Trebuchet.cs
+++ 1/Trebuchet.cs
@@ -6,9 +6,11 @@ using System.Text.RegularExpressions;
 string filePath = "1input.txt";
 try {
     string[] lines = Trebuchet.ReadFileLines(filePath);
+    int totalSum = 0;
     foreach (string line in lines) {
-        Console.WriteLine(line);
+        totalSum += Trebuchet.ProcessDemLinesFoo(line);
     }
+    Console.WriteLine($"here is the answer?!??: {totalSum}");
 }
 catch (Exception exceptionallyCute) {
     Console.WriteLine($"we done messed up: {exceptionallyCute.Message}");
@@ -17,5 +19,23 @@ catch (Exception exceptionallyCute) {
 public class Trebuchet {
     public static string[] ReadFileLines(string filePath) {
         return File.ReadAllLines(filePath);
+    }
+
+    public static int ProcessDemLinesFoo(string line)
+    {
+        Regex reggedUp = new Regex(@"\d");
+        MatchCollection matches = reggedUp.Matches(line);
+
+        if (matches.Count >= 2)
+        {
+            int firstDiglett = int.Parse(matches[0].Value);
+            int lastDiglett = int.Parse(matches[matches.Count - 1].Value);
+            int result = firstDiglett * 10 + lastDiglett;
+        
+            Console.WriteLine($"Processing line: '{line}', First Digit: {firstDiglett}, Last Digit: {lastDiglett}, Result: {result}");
+            return result;
+        }
+
+        return 0;
     }
 }
