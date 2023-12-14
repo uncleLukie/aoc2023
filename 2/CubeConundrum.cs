542e7c8e0bbbe75b50f1be6ccf0d0788d82e021a
Luke Hewitt <lukehewitt@smashit.games>
2023-12-15 09:16:43 +1000

    im stuck rip

--- 2/CubeConundrum.cs
+++ 2/CubeConundrum.cs
@@ -1,6 +1,67 @@
-namespace DefaultNamespace;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
-public class CubeConundrum
+
+try
+    {
+        CubeConundrum cubeyBoi = new CubeConundrum();
+        int lolSkeet = cubeyBoi.GetSumOfPossibleGames(12, 13, 14);
+        Console.WriteLine($"sum of possy games: {lolSkeet}");
+    }
+catch (Exception exceptionallySnowy)
+    {
+        Console.WriteLine($"we done messed up: {exceptionallySnowy.Message}");
+    }
+
+    public class CubeConundrum
 {
+    private Dictionary<int, List<CubeDraw>> games = new Dictionary<int, List<CubeDraw>>();
+
+    public void AddGame(int gameId, List<CubeDraw> draws)
+    {
+        games.Add(gameId, draws);
+    }
+
+    public bool IsGamePossible(int gameId, int redCubes, int greenCubes, int blueCubes)
+    {
+        if (!games.ContainsKey(gameId))
+        {
+            return false;
+        }
+
+        List<CubeDraw> draws = games[gameId];
+
+        foreach (var draw in draws)
+        {
+            if (draw.Red > redCubes || draw.Green > greenCubes || draw.Blue > blueCubes)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int GetSumOfPossibleGames(int redCubes, int greenCubes, int blueCubes)
+    {
+        int sumOfGameIds = 0;
+
+        foreach (var game in games)
+        {
+            if (IsGamePossible(game.Key, redCubes, greenCubes, blueCubes))
+            {
+                sumOfGameIds += game.Key;
+            }
+        }
+        return sumOfGameIds;
+    }
     
+}
+
+public class CubeDraw
+{
+    public int Red { get; set; }
+    public int Green { get; set; }
+    public int Blue { get; set; }
 }
